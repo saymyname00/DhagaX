04e2f2a79675202a8c64022c041fe8ede42ddce3
Laughing Beggar <sajukaju930@gmail.com>
2026-03-11 23:42:40 +0530

    Update comment for clarity in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,5 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Do not modify arrays; keep Expo defaults to avoid Node compatibility issues
+// No array modification to avoid Node compatibility issues
 module.exports = config;
