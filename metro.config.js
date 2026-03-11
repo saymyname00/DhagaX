80431f1bc50b95e282ec4d24c7d9098fc0ee61b1
Laughing Beggar <sajukaju930@gmail.com>
2026-03-11 22:33:47 +0530

    Update metro.config.js for compatibility and fixes
    Updated import path for metro-config and added a fix for older Node environments to reverse source extensions.

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
+
+// Fix for older Node environments
+if (Array.prototype.toReversed) {
+  config.resolver.sourceExts = config.resolver.sourceExts.toReversed();
+} else {
+  config.resolver.sourceExts = config.resolver.sourceExts.slice().reverse();
+}
 
 module.exports = config;
